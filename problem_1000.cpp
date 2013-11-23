136107d2cb7acd9ef7003e04125b0c30379f9695
khurshid-normuradov <khurshid.normuradov@gmail.com>
2013-11-23 14:10:23 +0500

    Create problem_1000.cpp
    There is a solution of acm.timus.ru 1000-th problem.

---
+++ problem_1000.cpp
@@ -0,0 +1,27 @@
+/***
+*
+* @author Khurshid.Normuradov
+* @file   problem_1000.cpp
+* @date   23-Nov.2013.
+* @remark There is a solution of acm.timus.ru 1000-th problem.
+*/
+
+
+#include <cstdio>
+
+int main()
+{
+    // two numbers
+    int a , b  ;
+    
+    // read the numbers
+    scanf("%d %d",&a,&b);
+    
+    //calc sum of our numbers
+    int c = a + b;
+    
+    // print  sum 
+    printf("%d\n", c );
+    
+    return 0;
+}
